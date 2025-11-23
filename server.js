e4a830019a98d9872353098f8c54bef750551953
Pooja-tequity <pooja@tequity.tech>
2025-11-23 21:02:10 +0530

    add server listen

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const crypto = require('crypto');
 require('dotenv').config();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 4000
 const clients = new Set();
 
 function sendText(socket, str) {
